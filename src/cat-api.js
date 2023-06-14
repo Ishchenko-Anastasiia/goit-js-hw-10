3462e81ff43075eac02e938e309a6096098e82e3
Ishchenko-Anastasiia <123247356+Ishchenko-Anastasiia@users.noreply.github.com>
2023-06-14 14:28:19 +0300

    hw-10.1

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,7 +1,7 @@
 export function fetchBreeds() {
   const BASE_URL = 'https://api.thecatapi.com/v1';
   const API_KEY =
-    'live_LNtGUGvnkn0MleVjdqd4yIwVLGRn0fBWg7rGNFbKucUpq7ucAj5JRMkiJScrOFTW';
+    'live_eY7uZA0V6lz1nEBQxtdbXb7Vqlcd0HRi86atSBXRKFY1Ee91kedefMWMinETDUVA';
 
   return fetch(`${BASE_URL}/breeds?api-key=${API_KEY}`).then(response => {
     if (!response.ok) {
@@ -15,7 +15,7 @@ export function fetchBreeds() {
 export function fetchCatByBreed(breedId) {
   const BASE_URL = 'https://api.thecatapi.com/v1';
   const API_KEY =
-    'live_LNtGUGvnkn0MleVjdqd4yIwVLGRn0fBWg7rGNFbKucUpq7ucAj5JRMkiJScrOFTW';
+    'live_eY7uZA0V6lz1nEBQxtdbXb7Vqlcd0HRi86atSBXRKFY1Ee91kedefMWMinETDUVA';
 
   return fetch(
     `${BASE_URL}/images/search?breed_ids=${breedId}&api_key=${API_KEY}`
